5780e580f85924b89919a466e697f4516b7bfb19
xsarthak6 <sarthakjais612@gmail.com>
2026-07-01 14:39:22 +0530

    Integrate MongoDB Atlas with backend API

--- backend/models/Stay.js
+++ backend/models/Stay.js
@@ -1,25 +1,35 @@
 const mongoose = require("mongoose");
 
-const staySchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-  },
+const staySchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
 
-  location: {
-    type: String,
-    required: true,
-  },
+    location: {
+      type: String,
+      required: true,
+    },
 
-  price: {
-    type: Number,
-    required: true,
-  },
+    price: {
+      type: Number,
+      required: true,
+    },
 
-  image: {
-    type: String,
-    default: "",
+    image: {
+      type: String,
+      default: "",
+    },
+
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
   },
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("Stay", staySchema);
